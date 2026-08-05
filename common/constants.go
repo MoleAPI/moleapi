d874f379e97c8bb5f682845e5334231c8b4d76aa
Clarence Dan <tangdan0854@gmail.com>
2026-08-05 17:25:57 +0800

    chore(release): prepare 0.10.7-dev1

--- common/constants.go
+++ common/constants.go
@@ -11,8 +11,8 @@ import (
 )
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v0.10.6.8"         // this hard coding will be replaced automatically when building, no need to manually change
-var UpstreamVersion = "v1.0.0-rc.22"
+var Version = "v0.10.7-dev1"      // this hard coding will be replaced automatically when building, no need to manually change
+var UpstreamVersion = "v1.0.0-rc.23"
 var Commit = ""
 var SystemName = "New API"
 var Footer = ""
